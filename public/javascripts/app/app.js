a3d777086781f7c5c6f4a4624c6d7b40b2e5d0a7
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-11-16 21:27:32 -0600

    initial jade

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -7,6 +7,9 @@ var socket;
 function initialize(){
   $(document).foundation();
   initializeSocketIO();
+  $('#authentication-button').on('click', clickAuthenticationButton);
+  $('#register').on('click', clickRegister);
+  $('#login').on('click', clickLogin);
 }
 
 function initializeSocketIO(){
@@ -19,4 +22,73 @@ function initializeSocketIO(){
 
 function socketConnected(data){
   console.log(data);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+
+function clickAuthenticationButton(){
+  var isAnonymous = $('#authentication-button[data-email="anonymous"]').length === 1;
+
+  if(isAnonymous){
+    $('form#authentication').toggleClass('hidden');
+    $('input[name="email"]').focus();
+  } else {
+    var url = '/logout';
+    sendAjaxRequest(url, {}, 'post', 'delete', null, function(data){
+      htmlLogout(data);
+    });
+}
+
+function clickRegister(){
+  var url = '/users';
+  var data = $('form#authentication').serialize();
+  sendAjaxRequest(url, data, 'post', null, e, function(data){
+    htmlRegisterComplete(data);
+  });
+}
+
+function clickLogin(){
+  var url = '/login';
+  var data = $('form#authentication').serialize();
+  sendAjaxRequest(url, data, 'post', 'put', e, function(data){
+    htmlUpdateLoginStatus(data);
+  });
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+function htmlRegisterComplete(result){
+  $('input[name="email"]').val('');
+  $('input[name="password"]').val('');
+
+  if(result.status === 'ok'){
+    $('form#authentication').toggleClass('hidden');
+  }
+}
+
+function htmlLogout(data){
+  $('#authentication-button').attr('data-email', 'anonymous');
+  $('#authentication-button').text('Login | Sign Up');
+  $('#authentication-button').removeClass('alert');
+  $('#the-application').addClass('hidden');
+  window.location.href = '/';
+}
+
+function htmlUpdateLoginStatus(result){
+  $('input[name="email"]').val('');
+  $('input[name="password"]').val('');
+
+  if(result.status === 'ok'){
+    $('form#authentication').toggleClass('hidden');
+    $('#authentication-button').attr('data-email', result.email);
+    $('#authentication-button').text(result.email);
+    $('#authentication-button').addClass('alert');
+    $('#the-application').removeClass('hidden');
+    window.location.href = '/';
+  }
 }
