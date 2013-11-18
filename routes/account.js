e8a306f89989a62d8919e821c5e9d43b5d110041
Matthew Wiser <matt.k.wiser@gmail.com>
2013-11-18 14:57:16 -0600

    :smoking: passing and saving offer objects

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,14 @@
+var mongoose = require('mongoose');
+var Offer = mongoose.model('Offer');
+
 exports.create = function(req, res){
   res.render('account/create', {title: 'Rout.ly'});
 };
 
 exports.overview = function(req, res){
-  res.render('account/overview', {title: 'Rout.ly'});
+Offer.find(function(err, offers){
+  res.render('account/overview', {title: 'Rout.ly', offers: offers});
+  });
 };
 
 exports.new = function(req, res){
